f0cf1d6b19a5f47e9f4f60abd55f0fd4897371a2
MARCO <jmukhammadsodikh@gmail.com>
2026-06-05 15:54:53 +0900

    feat: create restaurant member related router get and post endpoints

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -7,6 +7,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
     try {
         console.log('goHome')
         res.send('Home Page');
+        // send | json | redirect | end | render
     }
     catch (err) {
         console.log("Error, goHome:", err)
@@ -33,4 +34,32 @@ restaurantController.getSignup = (req: Request, res: Response) => {
     }
 };
 
+restaurantController.getSignup = (req: Request, res: Response) => {
+    try {
+        console.log('getSignup')
+        res.send('Signup Page');
+    }
+    catch (err) {
+        console.log("Error, getSignup:", err)
+    }
+};
+restaurantController.processLogin = (req: Request, res: Response) => {
+    try {
+        console.log('processLogin')
+        res.send("DONE")
+    }
+    catch (err) {
+        console.log("Error, processLogin:", err)
+    }
+};
+
+restaurantController.processSignup = (req: Request, res: Response) => {
+    try {
+        console.log('processSignup')
+        res.send("DONE")
+    }
+    catch (err) {
+        console.log("Error, processSignup:", err)
+    }
+};
 export default restaurantController;
