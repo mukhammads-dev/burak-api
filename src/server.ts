96735b02be4077633eed7d09d0e925b06cdd7401
MARCO <jmukhammadsodikh@gmail.com>
2026-06-10 17:54:30 +0900

    feat: frontend related css, js and image creation

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,8 @@ mongoose.connect(process.env.MONGO_URL as string, {}) // Call
         console.log("MongoDB connection succeed");
         const PORT = process.env.PORT ?? 3008;
         app.listen(PORT, function () {
-            console.log(`The server is running successfully on port: ${PORT}`);
+            console.info(`The server is running successfully on port: ${PORT}`);
+            console.info(`Admin project on http://localhost:${PORT}/admin \n`)
         });
     })
     .catch((err) => console.log("ERROR on connection MongoDB", err));
