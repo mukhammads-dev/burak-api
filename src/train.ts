290b334a28e8963099c2038d0ba2e557dd4a864c
MARCO <jmukhammadsodikh@gmail.com>
2026-06-26 17:45:48 +0900

    feat: train.ts

--- src/train.ts
+++ src/train.ts
@@ -32,8 +32,58 @@
  */
 
 // MIT Tasks ===================
-/*
-TASK V
+
+/*TASK W
+
+Shunday function yozing, u o'ziga parametr sifatida
+yagona array va number qabul qilsin. Siz tuzgan function
+arrayni numberda berilgan uzunlikda kesib bo'laklarga
+ajratgan holatida qaytarsin.
+MASALAN: chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3);
+return [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]];
+
+Yuqoridagi namunada berilayotgan array ikkinchi parametr 3'ga
+asoslanib 3 bo'lakga bo'linib qaytmoqda. Qolgani esa o'z holati qolyapti
+*/
+
+
+function chunkArray(arr: number[], size: number) {
+   let result = [];
+
+   for (let i = 0; i < arr.length; i += size) {
+      result.push(arr.slice(i, i + size));
+   }
+
+   return result;
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*TASK V
 
 Shunday function yozing, uni string parametri bo'lsin.
 Va bu function stringdagi har bir harfni o'zi bilan
@@ -44,7 +94,6 @@ MASALAN: countChars("hello") return {h: 1, e: 1, l: 2, o: 1}
 Yuqoridagi misolda, 'hello' so'zi tarkibida
 qatnashgan harflar necha marotaba takrorlangini bilan
 object sifatida qaytarilmoqda.
-*/
 
 function countChars(prop: string) {
    let result: any = {};
@@ -61,33 +110,9 @@ function countChars(prop: string) {
 
 console.log(countChars("success"))
 
+*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-TASK U
+/*TASK U
 Shunday function tuzing, uni number parametri bo'lsin.
 Va bu function berilgan parametrgacha, 0'dan boshlab
 oraliqda nechta toq sonlar borligini aniqlab return qilsi.
@@ -112,8 +137,7 @@ function sumOdds(prop: number) {
 console.log(sumOdds(11))
 */
 
-/*
-TASK T
+/*TASK T
 Shunday function tuzing, u sonlardan tashkil topgan 2'ta array qabul qilsin.
 Va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin.
 
@@ -134,8 +158,7 @@ console.log(
 
 */
 
-/*
-S-TASK
+/*S-TASK
 Shunday function yozing, u numberlardan tashkil topgan list qabul qilsin 
 va osha numberlar orasidagi tushib qolgan sonni topib uni return qilsin
 MASALAN: missing_number([3, 0, 1]) return 2
@@ -156,8 +179,7 @@ function missing_number(prop: number[]): number {
 console.log(missing_number([3, 0, 1]));
 */
 
-/**
- * R-TASK
+/* R-TASK
 Shunday function yozing, u string parametrga ega bolsin. String "1+2" holatda pass 
 qilinganda string ichidagi sonlar yigindisini number holatda qaytarsin.
 MASALAN: calculate("1+3") return 4;
@@ -175,8 +197,7 @@ function calculate(prop: string) {
 console.log(calculate("1+3"))
 */
 
-/**
- TASK Q:
+/**TASK Q:
 Shunday function yozing, u 2 ta parametrga ega bo'lib
 birinchisi object, ikkinchisi string bo'lsin.
 Agar qabul qilinayotgan ikkinchi string, objectning
