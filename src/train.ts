faab6f26c8bb8797d6ebf0ead08f640f61a876ff
MARCO <jmukhammadsodikh@gmail.com>
2026-07-01 18:28:02 +0900

    feat: train.ts

--- src/train.ts
+++ src/train.ts
@@ -32,6 +32,76 @@
  */
 
 // MIT Tasks ===================
+/*
+TASK Y
+
+Shunday function yozing, uni 2'ta array parametri bo'lsin.
+Bu function ikkala arrayda ham ishtirok etgan bir xil
+qiymatlarni yagona arrayga joylab qaytarsin.
+
+   MASALAN: findIntersection([1, 2, 3], [3, 2, 0]) return [2, 3]
+
+Yuqoridagi misolda, argument sifatida berilayotgan array'larda
+o'xshash sonlar mavjud. Function'ning vazifasi esa ana shu
+ikkala array'da ishtirok etgan o'xshash sonlarni yagona arrayga
+joylab return qilmoqda.
+*/
+
+function findIntersection(arr1: number[], arr2: number[]) {
+   let result = [];
+   for (const num of arr1) {
+      if (arr2.includes(num)) {
+         result.push(num)
+      }
+
+   }
+   return result
+
+
+}
+
+console.log(findIntersection([1, 2, 3], [3, 2, 0]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /* TASK X
@@ -46,7 +116,7 @@ MASALAN: countOccurrences({ model: 'Bugatti', steer: { model: 'HANKOOK', size: 3
 
 Yuqoridagi misolda, birinchi argument object, ikkinchi argument 'model'.
    Funktsiya, shu ikkinchi argument 'model', birinchi argument object
-tarkibida kalit sifatida 2 marotaba takrorlanganligi uchun 2 soni return qilmoqda */
+tarkibida kalit sifatida 2 marotaba takrorlanganligi uchun 2 soni return qilmoqda
 
 function countOccurrences(
    obj: Record<string, any>,
@@ -79,30 +149,7 @@ const result = countOccurrences(
 );
 
 console.log(result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  */
 
 
 /*TASK W
