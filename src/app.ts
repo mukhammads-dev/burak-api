51e91c566f1a0d132bed10445633cffb2bcdfd26
MARCO <jmukhammadsodikh@gmail.com>
2026-07-07 20:48:45 +0900

    feat: develop authentication via tokens with cookies

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import path from "path";
 import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import { MORGAN_FORMAT } from "./libs/config";
 
 // TCP 2 sessionlar uchun storage yasadik
@@ -21,6 +22,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser()); // global integratsiya cookie parser
 app.use(morgan(MORGAN_FORMAT));
 
 /** 2-SESSIONS **/
