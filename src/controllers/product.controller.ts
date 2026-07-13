db025a37485b23fddd82580d7cd17d6fc748226d
MARCO <jmukhammadsodikh@gmail.com>
2026-07-13 16:15:00 +0900

    feat: develop getProduct Rest Api

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
-import { AdminRequest } from "../libs/types/member";
+import { AdminRequest, ExtendedRequest } from "../libs/types/member";
 import { threadCpuUsage } from "process";
 import { ProductInput, ProductInquiry } from "../libs/types/product";
 import { ProductCollection } from "../libs/enums/product.enum";
@@ -17,7 +17,7 @@ productController.getProducts = async (req: Request, res: Response) => {
     try {
         console.log("getProducts");
 
-        const { page, limit, order, productCollection, search } = req.query;
+        const { page, limit, order, productCollection, search } = req.query; 3
 
         const inquiry: ProductInquiry = { // object yaratish
             order: String(order), // "createdAt" — qaysi fieldga qarab tartiblash
@@ -46,7 +46,23 @@ productController.getProducts = async (req: Request, res: Response) => {
     }
 };
 
+productController.getProduct = async (req: ExtendedRequest, res: Response) => {
+    try {
+        console.log("getProduct");
 
+        const { id } = req.params;
+
+        const memberId = req.member?._id ?? null,
+            result = await productService.getProduct(memberId, String(id));
+
+        res.status(HttpCode.OK).json(result);
+    } catch (err) {
+        console.log("Error, getProduct:", err);
+
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+    }
+};
 
 
 /** BSSR============ */
